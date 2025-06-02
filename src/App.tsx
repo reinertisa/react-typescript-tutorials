77792122077d0e8b97087659277b9837e147504e
Isa Reinert <reinert.isa@gmail.com>
2025-06-01 21:19:17 -0700

    feat(website): useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-import Container1 from "./components/styles/Container1.tsx";
+
+import LoggedIn1 from "./usestate/LoggedIn1.tsx";
 
 function App() {
     return (
         <div>
-           <Container1 styles={{border: '1px solid black', padding: '1rem'}} />
+           <LoggedIn1 />
         </div>
     )
 }
