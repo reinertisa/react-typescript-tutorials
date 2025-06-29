96437db163c515994f45f378981788e77ccac491
Isa Reinert <reinert.isa@gmail.com>
2025-06-28 23:53:17 -0700

    refactor(website): update folder structure

--- src/html/CustomComp1.tsx
+++ src/html/CustomComp1.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import type Greet3 from "../components/Greet3.tsx";
+import type Greet3 from "../1-props/Greet3.tsx";
 
 export default function CustomComp1(props: React.ComponentProps<typeof Greet3>) {
     const {isLoggedIn, messageCount = 0, name} = props;
